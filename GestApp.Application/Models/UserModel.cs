5b7653f33acdbf39b506889ec061ec5eedefce7e
Daniele Grandolfo <dgrandolfo@kpmg.it>
2025-02-20 16:38:54 +0100

    Added Account Page

---
+++ GestApp.Application/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GestApp.Application.Models;
+
+public class UserCardModel
+{
+    public string? Id { get; set; }
+    public string? Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Role { get; set; }
+}
